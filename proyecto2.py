b498b1f403909cd63b83c8da116fc20f5c1b6db6
Chris Cuenca <chrisfred86@gmail.com>
2022-05-30 20:04:11 -0500

    Sumo el  primero número al resultado

--- proyecto2.py
+++ proyecto2.py
@@ -3,5 +3,6 @@
 numero1 = int(input("Introduce el primer numero: "))
 numero2 = int(input("Introduce el segundo numero: "))
 resultado = numero1 * numero2
-print ("El numero que ingreso es:\n",resultado)
+resultado_suma = resultado + numero1 #Agrego suma al resultado
+print ("El numero que ingreso es:\n",resultado_suma)
 

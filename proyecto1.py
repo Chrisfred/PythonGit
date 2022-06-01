77dd0820a8d49cf7418089c67abcaf76468f34fe
Chris Cuenca <chrisfred86@gmail.com>
2022-05-31 21:52:41 -0500

    Suma de número con datos de entrada

--- proyecto1.py
+++ proyecto1.py
@@ -8,3 +8,10 @@ num2_suma = 2
 resultado_suma = num1_suma + num2_suma
 
 print ("El resultado de la suma es: ", resultado_suma)
+
+#Suma con datos de ingreso
+
+numero1 = int(input("Introduce el primer numero: "))
+numero2 = int(input("Introduce el segundo numero: "))
+resultado = numero1 + numero2
+print ("El resulatdo es:\n",resultado)

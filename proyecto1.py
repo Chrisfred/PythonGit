d5fb0bc8c44cfe13804c4aa09d5df5f60e7f5b29
Chris Cuenca <chrisfred86@gmail.com>
2022-06-01 22:56:55 -0500

    mas código para probar el merge

--- proyecto1.py
+++ proyecto1.py
@@ -7,7 +7,7 @@ num1_suma = 2
 num2_suma = 2
 resultado_suma = num1_suma + num2_suma
 
-print ("El resultado de la suma es: ", resultado_suma, "y es un valor correcto, sin lugar alguna")
+print ("El resultado de la suma es: ", resultado_suma, "y es un valor correcto, sin lugar alguna alguna")
 
 #Suma con datos de ingreso
 
@@ -16,3 +16,4 @@ numero2 = int(input("Introduce el segundo numero: "))
 resultado = numero1 + numero2
 print ("El resulatdo es:\n",resultado)
 print ("Y cerramos el sistma")
+print ("Y cerramos el sistema")

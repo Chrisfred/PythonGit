3d3b4c131bbc829bd529108b9fbf58136698e08c
Chris Cuenca <chrisfred86@gmail.com>
2022-06-01 22:48:34 -0500

    agregamos código sin duda alguna  para validar fusión con la master

--- proyecto1.py
+++ proyecto1.py
@@ -7,7 +7,7 @@ num1_suma = 2
 num2_suma = 2
 resultado_suma = num1_suma + num2_suma
 
-print ("El resultado de la suma es: ", resultado_suma, "y es un valor correcto")
+print ("El resultado de la suma es: ", resultado_suma, "y es un valor correcto, sin lugar alguna")
 
 #Suma con datos de ingreso
 
@@ -15,3 +15,4 @@ numero1 = int(input("Introduce el primer numero: "))
 numero2 = int(input("Introduce el segundo numero: "))
 resultado = numero1 + numero2
 print ("El resulatdo es:\n",resultado)
+print ("Y cerramos el sistma")

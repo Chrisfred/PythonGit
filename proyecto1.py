5d69020e305ba9b7b1a2d05c3eb090627cc06bfa
Chris Cuenca <chrisfred86@gmail.com>
2022-06-03 22:13:16 -0500

    I add a automation on the automation branch

--- proyecto1.py
+++ proyecto1.py
@@ -7,4 +7,10 @@ num1_suma = 2
 num2_suma = 2
 resultado_suma = num1_suma + num2_suma
 
-print ("El resultado de la suma es: ", resultado_suma)
+print ("El resultado de la suma es: ", resultado_suma, "It's not a automation")
+
+a =  int (input ("ingrese el ptimer numero: "))
+b = int(input("Ingrese en el segundo numero: "))
+c = a + b
+print("El resultados", c, "This is a automation")
+

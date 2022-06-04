e56028eba39b6a4f21c27466144f86c6511aa13f
Chris Cuenca <chrisfred86@gmail.com>
2022-06-03 22:38:41 -0500

    I add a action plan on master branch

--- proyecto1.py
+++ proyecto1.py
@@ -7,7 +7,7 @@ num1_suma = 2
 num2_suma = 2
 resultado_suma = num1_suma + num2_suma
 
-print ("El resultado de la suma es: ", resultado_suma, "It's not a automation")
+print ("El resultado de la suma es: ", resultado_suma, "It's not a automation, pero voy hacer un plan de acción")
 
 a =  int (input ("ingrese el ptimer numero: "))
 b = int(input("Ingrese en el segundo numero: "))

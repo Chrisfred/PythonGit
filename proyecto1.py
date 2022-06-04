9e8295a4140f6473ff269b4c18baedf6375870e1
Chris Cuenca <chrisfred86@gmail.com>
2022-06-03 22:54:05 -0500

    I thinking about remove the action plan

--- proyecto1.py
+++ proyecto1.py
@@ -7,7 +7,7 @@ num1_suma = 2
 num2_suma = 2
 resultado_suma = num1_suma + num2_suma
 
-print ("El resultado de la suma es: ", resultado_suma, "It's not a automation, pero estamos en estudio de esta automatización")
+print ("El resultado de la suma es: ", resultado_suma, "It's not a automation, pero estoy pensando en retirar el plan de acción")
 
 a =  int (input ("ingrese el ptimer numero: "))
 b = int(input("Ingrese en el segundo numero: "))

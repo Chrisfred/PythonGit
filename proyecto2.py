cbc84d5b46e62b05762bfe235905ac3e190f5ccb
Chris Cuenca <chrisfred86@gmail.com>
2022-06-16 22:36:02 -0500

    Agrego un valor que resta al resultado

--- proyecto2.py
+++ proyecto2.py
@@ -4,5 +4,10 @@
 numero1 = int(input("Introduce el primer numero: "))
 numero2 = int(input("Introduce el segundo numero: "))
 resultado = numero1 - numero2
-print ("El numero que ingreso es:\n",resultado)
+print ("El resultado de la resta es:\n",resultado)
+
+a = int(input("Ahora ingresa un número que restará el valor del resulado: "))
+res = resultado -a
+print("El nuevo resulrado es: ", res)
+
 
